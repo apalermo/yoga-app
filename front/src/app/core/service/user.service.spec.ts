cfe38648ec123fbc2ff2ce04f643a9e9d2f512bd
apalermo <anthony.palermo@laposte.net>
2025-12-02 16:31:00 +0100

    test(front): add unit tests for UserService

--- front/src/app/core/service/user.service.spec.ts
+++ front/src/app/core/service/user.service.spec.ts
@@ -1,20 +1,64 @@
-import { provideHttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
 import { UserService } from './user.service';
+import { User } from '../models/user.interface';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve a user by ID', () => {
+    const mockUser: User = {
+      id: 1,
+      email: 'test@test.com',
+      lastName: 'Last',
+      firstName: 'First',
+      admin: false,
+      password: '12345678@A',
+      createdAt: new Date(),
+    };
+
+    service.getById('1').subscribe({
+      next: (user) => {
+        expect(user).toEqual(mockUser);
+      },
+    });
+
+    const req = httpMock.expectOne('api/user/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
+  });
+
+  it('should delete a user', () => {
+    service.delete('1').subscribe({
+      next: (response) => {
+        expect(response).toBeNull();
+      },
+    });
+
+    const req = httpMock.expectOne('api/user/1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
   });
 });
