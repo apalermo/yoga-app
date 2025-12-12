ea563a783ad702fc07ddd5640ee28c307a62170c
apalermo <anthony.palermo@laposte.net>
2025-12-12 10:17:44 +0100

    fix(security): enable AuthEntryPoint and implement equals/hashCode in UserDetails

--- back/src/main/java/com/openclassrooms/starterjwt/security/services/UserDetailsImpl.java
+++ back/src/main/java/com/openclassrooms/starterjwt/security/services/UserDetailsImpl.java
@@ -3,18 +3,21 @@ package com.openclassrooms.starterjwt.security.services;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 
 @Builder
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class UserDetailsImpl implements UserDetails {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
@@ -54,13 +57,4 @@ public class UserDetailsImpl implements UserDetails {
         return true;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        UserDetailsImpl user = (UserDetailsImpl) o;
-        return Objects.equals(id, user.id);
-    }
 }
