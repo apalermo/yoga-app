dfc319c81802ae48ad33fc1e96e214c54ac69c80
apalermo <anthony.palermo@laposte.net>
2025-12-12 10:19:43 +0100

    test(app): add application context loading test

---
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.starterjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
